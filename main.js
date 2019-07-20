1515da6644a0e6c3815dac4919468a65565f8ee9
Erie <ema82@cornell.edu>
2019-07-19 23:17:34 -0400

    better vectors and show curves

--- main.js
+++ main.js
@@ -11,53 +11,24 @@ var config = {
         update: update
     }
 };
-
+let numPoints = 15;
 var game = new Phaser.Game(config);
 const numRockets = 100;
 const gameState = {};
 class Rocket {
     constructor(genes) {
         this.genes = genes;
-        this.curve = new Phaser.Curves.CubicBezier(genes, genes[50], genes[100], genes[149]);
-        this.points = this.curve.getPoints(150);
+        this.curve = new Phaser.Curves.CubicBezier(genes[0], genes[1], genes[2], genes[3]);
+        this.points = this.curve.getPoints(numPoints);
         this.fitness = 0;
         this.crashed = false;
         this.inEarth = false;
     }
 }
-function test() {
-    let genes = createGenes2();
-
-    console.log(genes);
-    let curve1 =new Phaser.Curves.CubicBezier(genes[0], genes[1], genes[2], genes[3]);
-    for (let i = 2; i < 4; i++) {
-        genes[i].x = genes[i].x+ 50;
-        genes[i].y = genes[i].y +60;
-    }
-
-    console.log(genes);
-    let curve2 = new Phaser.Curves.CubicBezier(genes[0], genes[1], genes[2],genes[3]);
-    let p0 = new Phaser.Math.Vector2(800, 600);
-    let p1 = new Phaser.Math.Vector2(700, 300);
-    let p2 = new Phaser.Math.Vector2(100, 500);
-    let p3 = new Phaser.Math.Vector2(200, 150);
-    console.log(curve1);
-    console.log(curve2);
-    return [curve1,curve2];
-}
-
-function createGenes2() {
-    let genes = [];
-    genes.push(new Phaser.Math.Vector2(400, 600));
-    for (let i = 1; i<4; i++) {
-        genes.push(new Phaser.Math.Vector2(Math.random() * 800, Math.random() * 600));
-    }
-    return genes;
-}
 function createGenes() {
     let genes = [];
     genes.push(new Phaser.Math.Vector2(400, 600));
-    for (let i = 1; i<150; i++) {
+    for (let i = 1; i < 4; i++) {
         genes.push(new Phaser.Math.Vector2(Math.random() * 800, Math.random() * 600));
     }
     return genes;
@@ -104,11 +75,6 @@ function create () {
         gameState.rockets[i].setCollideWorldBounds(true);
 
     }
-    curves = test();
-    graphics = this.add.graphics();
-    graphics.lineStyle(1, 0xffffff, 1);
-    curves[0].draw(graphics, 150);
-    curves[1].draw(graphics, 150);
 
 }
 function getAngle(x1, y1, x2, y2) {
@@ -117,24 +83,24 @@ function getAngle(x1, y1, x2, y2) {
 let p = 0;
 function update() {
     p++;
-    // if (p === 1) {
-    //     graphics = this.add.graphics();
-    //     for (let i = 0; i < numRockets; i++) {
-    //         graphics.lineStyle(1, 0xffffff, 1);
-    //         gameState.rocketVectors[i].curve.draw(graphics, 150);
-    //     }
-    // }
-    // if (p===150) {
-    //     graphics.destroy();
-    // }
-    if (p < 150) {
+    if (p === 1) {
+        graphics = this.add.graphics();
+        for (let i = 0; i < numRockets; i++) {
+            graphics.lineStyle(1, 0xffffff, 1);
+            gameState.rocketVectors[i].curve.draw(graphics, numPoints);
+        }
+    }
+    if (p===numPoints) {
+        graphics.destroy();
+    }
+    if (p < numPoints) {
         for (let i = 0; i < numRockets; i++) {
             if (!gameState.rocketVectors[i].crashed) {
                 let points = gameState.rocketVectors[i].points[p];
                 gameState.rockets[i].x = points.x;
                 gameState.rockets[i].y = points.y;
                 let tempVec = new Phaser.Math.Vector2();
-                let t = gameState.rocketVectors[i].curve.getUtoTmapping(p / 150);
+                let t = gameState.rocketVectors[i].curve.getUtoTmapping(p / numPoints);
                 let tangent = gameState.rocketVectors[i].curve.getTangent(t);
                 tempVec.copy(tangent).scale(32).add(points);
                 gameState.rockets[i].angle = getAngle(tempVec.x, tempVec.y, points.x, points.y) - 90;
@@ -146,13 +112,14 @@ function update() {
             }
             if (!gameState.rocketVectors[i].crashed && insideBox(gameState.rockets[i])) {
                 gameState.rocketVectors[i].fitness = fitness(gameState.rockets[i]);
-                gameState.rocketVectors[i].fitness = gameState.rocketVectors[i].fitness/10;
+                gameState.rocketVectors[i].fitness = 0;
+                    // gameState.rocketVectors[i].fitness/30;
                 gameState.rocketVectors[i].crashed = true;
             }
             if (!gameState.rocketVectors[i].crashed && !insideBox(gameState.rockets[i])
                 && !gameState.rocketVectors[i].inEarth && inEarth(gameState.rockets[i])) {
                 gameState.rocketVectors[i].fitness = fitness(gameState.rockets[i]);
-                gameState.rocketVectors[i].fitness = gameState.rocketVectors[i].fitness * 10;
+                gameState.rocketVectors[i].fitness = gameState.rocketVectors[i].fitness * 30;
                 gameState.rocketVectors[i].inEarth = true;
                 gameState.rocketVectors[i].crashed = true;
             }
@@ -181,11 +148,10 @@ function update() {
         for (let i = 0; i < numRockets; i++) {
             let r1 = matingPool[Math.floor(Math.random() * matingPool.length - 1)];
             let r2 = matingPool[Math.floor(Math.random() * matingPool.length - 1)];
-            let r3 = matingPool[Math.floor(Math.random() * matingPool.length - 1)];
-            if (r1 === undefined || r2 === undefined || r3 === undefined) {
+            if (r1 === undefined || r2 === undefined) {
                 newRockets.push(matingPool[0]);
             } else {
-                newRockets.push(mate(r1, r2, r3));
+                newRockets.push(mate(r1, r2));
             }
         }
         gameState.rocketVectors = newRockets;
@@ -193,61 +159,56 @@ function update() {
         for (let i = 0; i < numRockets; i++) {
             gameState.rockets[i].x = 400;
             gameState.rockets[i].y = 600;
-            gameState.rockets[i].visible = true;
         }
     }
 }
 function mutate(rocket) {
     let genes = rocket.genes;
-    // console.log(rocket);
-    // console.log(genes);
-    // console.log(genes[149]);
     let rand = Math.floor(Math.random() * (genes.length - 1 ));
-    let rand2 = Math.floor(Math.random() * (genes.length - 1 ));
+    let rand2 = Math.floor(Math.random() * (genes.length - 1 ) * 2);
     let p1;
-    let p2;
     for (let i = 1; i < genes.length; i++) {
-        if (i <= rand + 10 && i >= rand - 10) {
-            genes[i] = Phaser.Math.Vector2(Math.random() * 800, Math.random() * 600);
+        if (i === rand) {
             rand = Math.floor(Math.random() * (genes.length - 1 ));
             p1 = new Phaser.Math.Vector2(Math.random() * 800, Math.random() * 600);
-            p2 = new Phaser.Math.Vector2(Math.random() * 800, Math.random() * 600);
+            genes[i] = p1;
         }
-        if (i <= rand2 + 10 && i >= rand2 - 10) {
-            let distToEarth = Math.floor(Math.sqrt((Math.pow(genes[149].x - 410, 2)) + Math.pow(genes[149].y - 100, 2)));
-            if (genes[149].x < 410) {
-                genes[149].x += distToEarth/3;
+    }
+    for (let i = 1; i < genes.length * 2; i++) {
+        if (i === rand2) {
+            let distToEarth = Math.floor(Math.sqrt((Math.pow(genes[3].x - 410, 2)) + Math.pow(genes[3].y - 100, 2)));
+            let newX;
+            let newY;
+            if (genes[3].x < 410) {
+                newX = genes[3].x + distToEarth/3;
             } else {
-                genes[149].x -= distToEarth/3;
+                newX = genes[3] - distToEarth/3;
             }
-            if (genes[149] < 100) {
-                genes[149].y += distToEarth/3;
+            if (genes[3].y < 100) {
+                newY = genes[3].y + distToEarth/3;
             } else {
-                genes[149].y -= distToEarth/3;
+                newY = genes[3].y - distToEarth/3;
             }
+            genes[3] = new Phaser.Math.Vector2(newX, newY);
         }
-
     }
-    if (p1 !== undefined && p2 !== undefined) {
-        rocket.curve = new Phaser.Curves.CubicBezier(genes, p1, p2, genes[149]);
-    }
+    rocket.genes = genes;
+    rocket.curve = new Phaser.Curves.CubicBezier(genes[0], genes[1], genes[2], genes[3]);
+    return rocket;
 }
-let rateOfMutation = 100;
-function mate(rocket1, rocket2, rocket3) {
-    if (rocket1 === undefined || rocket2 === undefined || rocket3 === undefined) {
+let rateOfMutation = 1;
+function mate(rocket1, rocket2) {
+    if (rocket1 === undefined || rocket2 === undefined) {
         return undefined;
     }
-    let pos = Math.floor(Math.random() * 149);
-    let pos2 = Math.floor(Math.random() * 149);
+    let pos = Math.floor(Math.random() * 3);
     let genes;
-    if (pos < pos2) {
-        genes = rocket1.genes.slice(0,pos).concat(rocket2.genes.slice(pos, pos2)).concat(rocket3.genes.slice(pos2, 150));
-    } else {
-        genes = rocket1.genes.slice(0,pos2).concat(rocket2.genes.slice(pos2, pos)).concat(rocket3.genes.slice(pos, 150));
-    }
+    genes = rocket1.genes.slice(0,pos).concat(rocket2.genes.slice(pos, 4));
     let rocket = new Rocket(genes);
     if ((Math.random() * 100) < rateOfMutation) {
-        mutate(rocket);
+        // console.log(rocket);
+        rocket = mutate(rocket);
+        // console.log(rocket);
     }
     return rocket;
 }
